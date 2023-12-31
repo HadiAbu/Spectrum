f209d3d91b52fb534c3e51fc365c6b833af453f8
HadiAbu <hadi.abuhamed@gmail.com>
2023-12-31 13:08:54 +0000

    update

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import App2 from "./App2";
+import App3 from "./App3";
 import "./index.css";
 import "devextreme/dist/css/dx.light.css";
 
@@ -11,5 +12,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     {/* <App /> */}
     {/* Assignment 2 */}
     <App2 />
+    {/* <App3 /> */}
   </React.StrictMode>
 );
